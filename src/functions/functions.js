3d27c663cc0aa0742db6d107c31afd704f0617f4
marcoduartestorm <marcosbottocomp@gmail.com>
2025-09-13 19:52:14 -0300

    Solution

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,48 +1,55 @@
-export function preenche(state) { // Preenche tabela
-    for (let i = 0; i < state.length; i++) {
-        const row = document.querySelectorAll('.field-row')[i];
+export function preenche(state) {
+  // Preenche tabela
+  for (let i = 0; i < state.length; i++) {
+    const row = document.querySelectorAll('.field-row')[i];
 
-        for (let j = 0; j < state.length; j++) {
-            const cell = row.querySelectorAll('.field-cell')[j];
+    for (let j = 0; j < state.length; j++) {
+      const cell = row.querySelectorAll('.field-cell')[j];
 
-            if (state[i][j] % 2 === 0 && state[i][j] >= 2 && state[i][j] <= 2048) {
-              cell.classList.add(`field-cell--${state[i][j]}`);
-              cell.textContent = state[i][j];
-            } else {
-              let vet = [];
-              vet = cell.className.split(' ');
+      if (state[i][j] % 2 === 0 && state[i][j] >= 2 && state[i][j] <= 2048) {
+        cell.classList.add(`field-cell--${state[i][j]}`);
+        cell.textContent = state[i][j];
+      } else {
+        let vet = [];
 
-              if (vet.length > 1) {
-                for (let i = 1; i < vet.length; i++) {
-                  cell.classList.remove(vet[i]);
-                }
-              }
+        vet = cell.className.split(' ');
 
-              cell.textContent = state[i][j];
-            }
+        if (vet.length > 1) {
+          for (let k = 1; k < vet.length; k++) {
+            cell.classList.remove(vet[k]);
+          }
         }
-    }
-};
 
-export function showMessage(type) { // Mostra menssagem
-  document.querySelectorAll('.message').forEach(msg => {
+        cell.textContent = state[i][j];
+      }
+    }
+  }
+}
+
+export function showMessage(type) {
+  // Mostra menssagem
+  document.querySelectorAll('.message').forEach((msg) => {
     msg.classList.add('hidden');
   });
 
   const messageToShow = document.querySelector(`.message-${type}`);
+
   if (messageToShow) {
     messageToShow.classList.remove('hidden');
   }
 }
 
-export function hideMessages() { // Mostra menssagem
-  document.querySelectorAll('.message').forEach(msg => {
+export function hideMessages() {
+  // Mostra menssagem
+  document.querySelectorAll('.message').forEach((msg) => {
     msg.classList.add('hidden');
   });
 }
 
-export function changeScore(score) { // Mostra menssagem
+export function changeScore(score) {
+  // Mostra menssagem
   const scoreTemp = document.querySelector('.game-score');
+
   scoreTemp.textContent = score;
 }
 
@@ -57,18 +64,19 @@ export function randomNumber() {
 }
 
 export function randomPosition(table) {
-  let vet = [];
+  const vet = [];
 
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       if (table[i][j] === 0) {
-        vet.push({ row: [i], column: [j]});
+        vet.push({ row: [i], column: [j] });
       }
     }
   }
 
-  let index1 = Math.floor(Math.random() * vet.length);
+  const index1 = Math.floor(Math.random() * vet.length);
   const obj = {};
+
   obj.row = vet[index1].row;
   obj.column = vet[index1].column;
 
